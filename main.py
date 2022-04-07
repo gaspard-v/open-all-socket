bfbe4293babfe50c6b4d127c31d4f300cd718df0
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2022-04-07 15:14:47 +0200

    modif

--- main.py
+++ main.py
@@ -4,23 +4,12 @@
 import sys
 import socket_creation
 from utils import printd
+
+
 def main():
-    def close_sockets(socket_list: list) -> None:
-        for counter, socket in enumerate(socket_list):
-            try:
-                socket.close()
-                printd(f'Socket {counter} closed')
-            except:
-                print(f'unable to close socket {counter}', file=sys.stderr)
-    socket_list = []
-    for port in range(1, 10):
-        try:
-            socket_list.append(socket_creation.create_listen_socket('::1', port))
-            printd(f'Socket port {port} created')
-        except Exception as err:
-            print(f'unable to create socket on port {port}\
-                   \nError message: {err}\n', file=sys.stderr)
-    close_sockets(socket_list)
+    socket_list = socket_creation.create_all_socket('::1', (10, 20))
+
+    socket_creation.close_sockets(socket_list)
 
 
 if __name__ == '__main__':
--- socket_creation.py
+++ socket_creation.py
@@ -11,13 +11,16 @@
 
 import socket
 from typing import Union, List
+from utils import printd
+import sys
 
 TCP = socket.SOCK_STREAM
 UDP = socket.SOCK_DGRAM
 RAW = socket.SOCK_RAW
 
-def create_listen_socket(ip: str, 
-                         port: int, 
+
+def create_listen_socket(ip: str,
+                         port: int,
                          socket_type: socket.SocketKind = TCP,
                          socket_setsockopt: List[int] = [socket.SO_REUSEADDR]) -> socket.socket:
     def get_ip_type(ip: str = ip) -> tuple[socket.AddressFamily, str]:
@@ -32,3 +35,32 @@ def create_listen_socket(ip: str,
     s.bind((address, port))
     s.listen(1)
     return s
+
+
+def close_sockets(socket_list: List[socket.socket]) -> None:
+    for socket in socket_list:
+        address, port, *rest = socket.getsockname()
+        try:
+            socket.close()
+            printd(f'Socket port {port} closed')
+        except:
+            print(f'unable to close socket port {port}', file=sys.stderr)
+
+
+def create_all_socket(ip: str, port: Union[int, tuple[int, int]], *args) -> List[socket.socket]:
+    sockets = []
+    min, max = 0, 0
+    if isinstance(port, int):
+        min, max = port, port
+    elif isinstance(port, tuple):
+        min, max = port
+    else:
+        raise TypeError('port must be int or tuple')
+    for port in range(min, max + 1):
+        try:
+            sockets.append(create_listen_socket(ip, port))
+            printd(f'Socket port {port} created')
+        except Exception as err:
+            print(f'unable to create socket on port {port}\
+                        \nError message: {err}\n', file=sys.stderr)
+    return sockets
