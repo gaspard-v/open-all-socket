59ac3dc270591674b28f70dde11a4fc78685098c
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2022-04-07 11:40:39 +0200

    create_listen_socket works

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
 import socket_creation
 def main():
-    socket_creation.create_socket(1, 2, (socket_creation.TCP))
+    try:
+        socket_creation.create_listen_socket('::1', 33426)
+    except Exception as err:
+        print(f'{err}', file=sys.stderr)
+        exit(1)
 
 if __name__ == '__main__':
     main()
--- socket_creation.py
+++ socket_creation.py
@@ -16,13 +16,19 @@ TCP = socket.SOCK_STREAM
 UDP = socket.SOCK_DGRAM
 RAW = socket.SOCK_RAW
 
-def create_socket(ip: str, 
+def create_listen_socket(ip: str, 
                   port: int, 
-                  socket_type: Union[TCP, UDP, RAW] = TCP,
+                  socket_type: socket.SocketKind = TCP,
                   socket_setsockopt: List[int] = [socket.SO_REUSEADDR]) -> socket.socket:
-    s = socket.socket(socket.AF_INET, socket_type)
+    def get_ip_type(ip: str = ip) -> tuple[socket.AddressFamily, str]:
+        ip_type = socket.getaddrinfo(ip, None)
+        address_family = ip_type[0][0]
+        address = ip_type[0][4][0]
+        return (address_family, address)
+    address_family, address = get_ip_type()
+    s = socket.socket(address_family, socket_type)
     for opt in socket_setsockopt:
         s.setsockopt(socket.SOL_SOCKET, opt, 1)
-    s.bind((ip, port))
+    s.bind((address, port))
     s.listen(1)
     return s
