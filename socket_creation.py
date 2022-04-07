fb0298dfd6bcda24a9ed1ae2b6ab792c02c7c0ff
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2022-04-07 09:48:04 +0200

    modif

--- socket_creation.py
+++ socket_creation.py
@@ -10,15 +10,19 @@
 # https://pubs.opengroup.org/onlinepubs/000095399/functions/setsockopt.html
 
 import socket
+from typing import Union, List
 
 TCP = socket.SOCK_STREAM
 UDP = socket.SOCK_DGRAM
 RAW = socket.SOCK_RAW
 
-def create_socket(ip, port, options: list[socket.SocketKind]):
-    pass
-    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    # s.bind((ip, port))
-    # s.listen(1)
-    # return s
+def create_socket(ip: str, 
+                  port: int, 
+                  socket_type: Union[TCP, UDP, RAW] = TCP,
+                  socket_setsockopt: List[int] = [socket.SO_REUSEADDR]) -> socket.socket:
+    s = socket.socket(socket.AF_INET, socket_type)
+    for opt in socket_setsockopt:
+        s.setsockopt(socket.SOL_SOCKET, opt, 1)
+    s.bind((ip, port))
+    s.listen(1)
+    return s
